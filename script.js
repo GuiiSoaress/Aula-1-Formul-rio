53f5c8427d367314c4218988851778cb489f7aff
Guilherme Soares <165791068+GuiiSoaress@users.noreply.github.com>
2024-06-21 09:49:51 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function ValidateForm(){
+    var fname = document.getElementById('fname').value.trim();
+    var email = document.getElementById('email').value.trim();
+    var message = document.getElementById('message').value.trim();
+
+    if(fname === ''){
+        alert('Preencha o campo nome!');
+        return false;
+    }
+
+    if(email === ''){
+        alert('Preencha o campo E-mal!');
+        return false;
+    }
+
+    if(message === ''){
+        alert('Preencha o campo Mensagem!');
+        return false;
+    }
+}
